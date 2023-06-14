683703fc5c8253fe25c59f4f6f1ef5b69c57db95
Dawood <mohammadddawoodtrumboo@gmail.com>
2023-06-14 16:47:36 +0530

    JS file added and first functionality added

---
+++ app.js
@@ -0,0 +1,21 @@
+var talkBtn = document.querySelector(".head-btn");
+console.log(talkBtn);
+
+// console.log(targetPos);
+talkBtn.addEventListener('click',function(){
+    var targetSection = document.getElementById("contact");
+    var targetPos = targetSection.getBoundingClientRect().top;
+    
+    var interval = setInterval(function(){
+        if(targetPos<=0){
+            clearInterval(interval);
+            return;
+        }
+        window.scrollBy(0,50);
+        targetPos-=50;
+    },12);
+
+
+});
+
+
