8da045a7f38b20c0e5751b223421f3080b71697b
Dawood <mohammadddawoodtrumboo@gmail.com>
2023-06-15 18:26:10 +0530

    push

--- app.js
+++ app.js
@@ -6,14 +6,19 @@ talkBtn.addEventListener('click',function(){
     var targetSection = document.getElementById("contact");
     var targetPos = targetSection.getBoundingClientRect().top;
     
-    var interval = setInterval(function(){
-        if(targetPos<=0){
-            clearInterval(interval);
-            return;
-        }
-        window.scrollBy(0,20);
-        targetPos-=20;
-    },0);
+    window.scrollTo({
+        top:targetSection.offsetTop,
+        behavior:'smooth'
+    });
+    
+    // var interval = setInterval(function(){
+    //     if(targetPos<=0){
+    //         clearInterval(interval);
+    //         return;
+    //     }
+    //     window.scrollBy(0,20);
+    //     targetPos-=20;
+    // },0);
 
 
 });
