cb30e2cb54ccdce0252201dc873dacd0d0c6311d
Dawood <mohammadddawoodtrumboo@gmail.com>
2023-06-17 12:54:09 +0530

    Slider added

---
+++ slider.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  slidesPerGroup: 2,
+ 
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    }
+  }
+  
+});
+
+// function setMobileClass() {
+//   var container = document.querySelector('.swiper-container');
+//   if (window.innerWidth < 768) {
+//     container.classList.add('mobile');
+//   } else {
+//     container.classList.remove('mobile');
+//   }
+// }
+
+// // Call the function when the window is resized
+// window.addEventListener('resize', setMobileClass);
+
+// // Set the initial class on page load
+// setMobileClass();
