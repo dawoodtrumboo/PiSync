3df2983b3ef74bbe56bfb89977724a61c6a498ff
Dawood Trumboo <mohammaddawoodtrumboo@gmail.com>
2023-06-20 02:35:03 +0530

    Testimonial slider finished

--- slider.js
+++ slider.js
@@ -15,35 +15,25 @@
   },
   breakpoints:{
     1050: {
-      slidesPerView: 4,
-      slidesPerGroup: 4,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
     }
   }
   
 });
 }
 {
-  var swiper = new Swiper(".testo-js", {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    slidesPerGroup: 1,
-   
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  const swiper = new Swiper('.js-testimonials-slider', {
+    grabCursor:true,
+    spaceBetween:30,
+    pagination:{
+        el: '.js-testimonials-pagination',
+        clicable:true
     },
     breakpoints:{
-      650: {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-      }
+        767:{
+            slidesPerView:2
+        }
     }
-    
-  });
+});
 }
